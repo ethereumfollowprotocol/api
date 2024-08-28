8903bf92277d5b397c6bcff0813acbbd777f0f2c
0xthrpw <0xthrpw@gmail.com>
2024-08-27 22:30:19 -0400

    param user account full

--- src/router/api/v1/users/account/index.ts
+++ src/router/api/v1/users/account/index.ts
@@ -1,5 +1,6 @@
 import type { Hono } from 'hono'
 import { env } from 'hono/adapter'
+import { includeValidator } from '#/router/api/v1/leaderboard/validators'
 import type { Services } from '#/service'
 import type { IEFPIndexerService } from '#/service/efp-indexer/service'
 import type { ENSProfile } from '#/service/ens-metadata/types'
@@ -7,8 +8,9 @@ import type { Address, Environment } from '#/types'
 import { isAddress } from '#/utilities'
 
 export function account(lists: Hono<{ Bindings: Environment }>, services: Services) {
-  lists.get('/:addressOrENS/account', async context => {
+  lists.get('/:addressOrENS/account', includeValidator, async context => {
     const { addressOrENS } = context.req.param()
+    const { include } = context.req.valid('query')
     const ensService = services.ens(env(context))
     const efp: IEFPIndexerService = services.efp(env(context))
     const returnedAddress: Address = await ensService.getAddress(addressOrENS)
@@ -16,10 +18,13 @@ export function account(lists: Hono<{ Bindings: Environment }>, services: Servic
       return context.json({ response: 'ENS name not valid or does not exist' }, 404) // return error if address is not valid
     }
     const { address, ...ens }: ENSProfile = await ensService.getENSProfile(returnedAddress.toLowerCase())
-    const primaryList = await efp.getUserPrimaryList(address)
-
     const response = { address } as Record<string, unknown>
-    // const is_primary_list = (primaryList ? Number.parseInt(primaryList.toString()) : null) === Number.parseInt(token_id)
-    return context.json({ ...response, ens, primary_list: primaryList?.toString() ?? null }, 200)
+
+    if (include === 'full') {
+      const primaryList = await efp.getUserPrimaryList(address)
+      return context.json({ ...response, ens, primary_list: primaryList?.toString() ?? null }, 200)
+    }
+
+    return context.json({ ...response, ens }, 200)
   })
 }
