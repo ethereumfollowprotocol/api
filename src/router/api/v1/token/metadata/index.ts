57b77ce87e4a070876e5078c1080df3a741d8e77
0xthrpw <0xthrpw@gmail.com>
2024-09-30 10:22:09 -0400

    token meta

--- src/router/api/v1/token/metadata/index.ts
+++ src/router/api/v1/token/metadata/index.ts
@@ -6,36 +6,36 @@ import type { ENSProfile } from '#/service/ens-metadata/types'
 import type { Environment } from '#/types'
 import type { Address } from '#/types/index'
 
-export function metadata(token: Hono<{ Bindings: Environment }>, services: Services): Hono<{ Bindings: Environment }> {
-  token.get('/metadata/:token_id', async context => {
+export function metadata(token: Hono<{ Bindings: Environment }>, _services: Services): Hono<{ Bindings: Environment }> {
+  token.get('/metadata/:token_id', context => {
     const { token_id } = context.req.param()
     if (Number.isNaN(Number(token_id))) {
       return context.json({ response: 'Invalid list id' }, 400)
     }
-    const ensService = services.ens(env(context))
-    const efp: IEFPIndexerService = services.efp(env(context))
-    const listUser: Address | undefined = await services.efp(env(context)).getAddressByList(token_id)
-    if (!listUser) {
-      return context.json({ response: 'Not Found' }, 404)
-    }
-    const { address, ...ens }: ENSProfile = await ensService.getENSProfile(listUser.toLowerCase())
-    const primaryList = await efp.getUserPrimaryList(address)
-    const isPrimary = primaryList?.toString() === token_id
+    // const ensService = services.ens(env(context))
+    // const efp: IEFPIndexerService = services.efp(env(context))
+    // const listUser: Address | undefined = await services.efp(env(context)).getAddressByList(token_id)
+    // if (!listUser) {
+    //   return context.json({ response: 'Not Found' }, 404)
+    // }
+    // const { address, ...ens }: ENSProfile = await ensService.getENSProfile(listUser.toLowerCase())
+    // const primaryList = await efp.getUserPrimaryList(address)
+    // const isPrimary = primaryList?.toString() === token_id
 
     // const ranksAndCounts = await efp.getUserRanksCounts(address)
-    const data = {
-      ens: { address, ...ens },
-      //   ranks: {
-      //     mutuals_rank: ranksAndCounts.mutuals_rank,
-      //     followers_rank: ranksAndCounts.followers_rank,
-      //     following_rank: ranksAndCounts.following_rank,
-      //     top8_rank: ranksAndCounts.top8_rank,
-      //     blocks_rank: ranksAndCounts.blocks_rank
-      //   },
-      //   followers_count: ranksAndCounts.followers,
-      //   following_count: ranksAndCounts.following,
-      is_primary: isPrimary
-    }
+    // const data = {
+    //   ens: { address, ...ens },
+    //   //   ranks: {
+    //   //     mutuals_rank: ranksAndCounts.mutuals_rank,
+    //   //     followers_rank: ranksAndCounts.followers_rank,
+    //   //     following_rank: ranksAndCounts.following_rank,
+    //   //     top8_rank: ranksAndCounts.top8_rank,
+    //   //     blocks_rank: ranksAndCounts.blocks_rank
+    //   //   },
+    //   //   followers_count: ranksAndCounts.followers,
+    //   //   following_count: ranksAndCounts.following,
+    //   is_primary: isPrimary
+    // }
     const url = context.req.url
     const rootUrl = url.split('/token/metadata')[0]
     const metadata = {
@@ -44,14 +44,14 @@ export function metadata(token: Hono<{ Bindings: Environment }>, services: Servi
       image: `${rootUrl}/token/image/${token_id}`,
       external_url: `https://testing.ethfollow.xyz/${token_id}`,
       attributes: [
-        {
-          trait_type: 'User',
-          value: data.ens.name
-        },
-        {
-          trait_type: 'Primary List',
-          value: data.is_primary
-        }
+        // {
+        //   trait_type: 'User',
+        //   value: data.ens.name
+        // },
+        // {
+        //   trait_type: 'Primary List',
+        //   value: data.is_primary
+        // }
         // {
         //   trait_type: 'Followers',
         //   value: data.followers_count
