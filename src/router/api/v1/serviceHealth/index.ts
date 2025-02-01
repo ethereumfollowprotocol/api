efa40c730018b15305a1e07d89cc7858a829c1dc
0xthrpw <0xthrpw@gmail.com>
2025-01-31 21:25:51 -0500

    service health

--- src/router/api/v1/serviceHealth/index.ts
+++ src/router/api/v1/serviceHealth/index.ts
@@ -7,24 +7,24 @@ export function serviceHealth(services: Services): Hono<{ Bindings: Environment 
   const serviceHealth = new Hono<{ Bindings: Environment }>()
 
   serviceHealth.get('/', async context => {
-    try{
-        const cacheService = services.cache(env(context))
-        const cacheTarget = `serviceHealth`
-        await cacheService.put(cacheTarget, JSON.stringify('ok'))
-        const cacheHit = await cacheService.get(cacheTarget)    
-        if(!cacheHit){
-            return context.json({ response: 'No Response from Cache Service' }, 500)
-        }
+    try {
+      const cacheService = services.cache(env(context))
+      const cacheTarget = `serviceHealth`
+      await cacheService.put(cacheTarget, JSON.stringify('ok'))
+      const cacheHit = await cacheService.get(cacheTarget)
+      if (!cacheHit) {
+        return context.json({ response: 'No Response from Cache Service' }, 500)
+      }
     } catch (e) {
       console.log(e)
       return context.json({ response: 'No Response from Cache Service' }, 500)
     }
 
-    try{
-        const response = await fetch(`${env(context).ENS_API_URL}/u/efp.eth`)
-        if(!response.ok){
-            return context.json({ response: 'No Response from ENS Service' }, 500)
-        }
+    try {
+      const response = await fetch(`${env(context).ENS_API_URL}/u/efp.eth`)
+      if (!response.ok) {
+        return context.json({ response: 'No Response from ENS Service' }, 500)
+      }
     } catch (e) {
       console.log(e)
       return context.json({ response: 'No Response from ENS Service' }, 500)
