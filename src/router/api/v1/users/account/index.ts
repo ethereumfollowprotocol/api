287639f2d5643d8079b24747be9f12e556197817
0xthrpw <0xthrpw@gmail.com>
2025-03-07 13:18:03 -0500

    acct cache

--- src/router/api/v1/users/account/index.ts
+++ src/router/api/v1/users/account/index.ts
@@ -10,9 +10,18 @@ import { isAddress } from '#/utilities'
 export function account(lists: Hono<{ Bindings: Environment }>, services: Services) {
   lists.get('/:addressOrENS/account', includeValidator, async context => {
     const { addressOrENS } = context.req.param()
-    const { include } = context.req.valid('query')
+    const { cache, include } = context.req.query()
+
+    const cacheService = services.cache(env(context))
+    const cacheTarget = `users/${addressOrENS}/account?include=${include}`
+    if (cache !== 'fresh') {
+      const cacheHit = await cacheService.get(cacheTarget)
+      if (cacheHit) {
+        return context.json({ ...cacheHit }, 200)
+      }
+    }
+
     const ensService = services.ens(env(context))
-    const efp: IEFPIndexerService = services.efp(env(context))
     const returnedAddress: Address = await ensService.getAddress(addressOrENS)
     if (!isAddress(returnedAddress)) {
       return context.json({ response: 'ENS name not valid or does not exist' }, 404) // return error if address is not valid
@@ -20,11 +29,12 @@ export function account(lists: Hono<{ Bindings: Environment }>, services: Servic
     const { address, ...ens }: ENSProfile = await ensService.getENSProfile(returnedAddress.toLowerCase())
     const response = { address } as Record<string, unknown>
 
-    if (include === 'full') {
-      const primaryList = await efp.getUserPrimaryList(address)
-      return context.json({ ...response, ens, primary_list: primaryList?.toString() ?? null }, 200)
-    }
-
-    return context.json({ ...response, ens }, 200)
+    // if (include === 'full') {
+    //   const primaryList = await efp.getUserPrimaryList(address)
+    //   packagedResponse = { ...response, ens, primary_list: primaryList?.toString() ?? null }
+    // }
+    const packagedResponse = { ...response, ens }
+    await cacheService.put(cacheTarget, JSON.stringify(packagedResponse))
+    return context.json(packagedResponse, 200)
   })
 }
